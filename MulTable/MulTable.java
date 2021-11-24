3d4cd005e172af7f26330bd2ead11a72009a5617
outlier007 <1504126196@qq.com>
2021-11-24 22:45:44 +0800

    乘法口诀表

---
+++ MulTable/MulTable.java
@@ -0,0 +1,14 @@
+public class MulTable {
+	public static void main(String[] args) {
+		int i=0;
+		int j=0;
+		for(i=1;i<=9;i++) {
+			for(j=1;j<=i;j++) {
+				System.out.printf("%d*%d=%-4d",j,i,i*j);
+				if(i==j) {
+					System.out.printf("\n");
+				}
+			}	
+		}
+	}
+}
