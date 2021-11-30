f55274bfca006a0a32ad9c551cffadd1c65344a6
outlier007 <1504126196@qq.com>
2021-11-30 15:38:51 +0800

    零钱通

---
+++ SmallChange/src/com/lmclearn/smallchange/opp/SmallChangeSysApp.java
@@ -0,0 +1,16 @@
+package com.lmclearn.smallchange.opp;
+
+/*
+ *项目名: SmallChange
+ *文件名: SmallChangeApp
+ *创建者: LMC
+ *创建时间:2021/11/29 22:18
+ *描述:opp编程版零钱通
+ */
+
+public class SmallChangeSysApp {
+    public static void main(String[] args) {
+        SmallChangeSysOpp smallChangeSysOpp = new SmallChangeSysOpp();
+        smallChangeSysOpp.mainMenu();
+    }
+}
